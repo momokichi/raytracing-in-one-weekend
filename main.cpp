939d2faea620e1df543febdcc44f3ee34ff1b413
Suzuki Kensuke <s1811349@s.tsukuba.ac.jp>
2021-11-21 21:53:37 +0900

    use utility functions

--- main.cpp
+++ main.cpp
@@ -1,4 +1,8 @@
 #include <iostream>
+
+#include "color.h"
+#include "vec3.h"
+
 using namespace std;
 
 int main() {
@@ -7,16 +11,12 @@ int main() {
 
   cout << "P3\n" << image_width << " " << image_height << "\n255" << endl;
   for (int j = image_height - 1; j >= 0; --j) {
+    cerr << "\rScanlines remaining: " << j << " " << flush;
     for (int i = 0; i < image_width; ++i) {
-      auto r = double(i) / (image_width - 1);
-      auto g = double(j) / (image_height - 1);
-      auto b = 0.25;
-
-      int ir = static_cast<int>(255.999 * r);
-      int ig = static_cast<int>(255.999 * g);
-      int ib = static_cast<int>(255.999 * b);
-
-      cout << ir << " " << ig << " " << ib << endl;
+      color pixel_color(double(i) / (image_width - 1),
+                        double(j) / (image_height - 1), 0.25);
+      write_color(cout, pixel_color);
     }
   }
+  cerr << "\nDone." << endl;
 }
