c1fcdb7f6cb8ced5d4ec337b4bbbc8122165a0b6
Suzuki Kensuke <s1811349@s.tsukuba.ac.jp>
2021-11-28 00:03:48 +0900

    render a sphere

--- main.cpp
+++ main.cpp
@@ -1,20 +1,51 @@
 #include <iostream>
 
 #include "color.h"
+#include "ray.h"
 #include "vec3.h"
 
 using namespace std;
 
+bool hit_sphere(const point3& center, double radius, const ray& r) {
+  vec3 oc = r.origin() - center;
+  auto a = dot(r.direction(), r.direction());
+  auto b = 2.0 * dot(oc, r.direction());
+  auto c = dot(oc, oc) - radius * radius;
+  auto discriminant = b * b - 4 * a * c;
+  return (discriminant > 0);
+}
+
+color ray_color(const ray& r) {
+  if (hit_sphere(point3(0, 0, -1), 0.5, r)) return color(1, 0, 0);
+  vec3 unit_direction = unit_vector(r.direction());
+  auto t = 0.5 * (unit_direction.y() + 1.0);
+  return (1.0 - t) * color(1.0, 1.0, 1.0) + t * color(0.5, 0.7, 1.0);
+}
+
 int main() {
-  const int image_width = 256;
-  const int image_height = 256;
+  const auto aspect_ratio = 16.0 / 9.0;
+  const int image_width = 384;
+  const int image_height = static_cast<int>(image_width / aspect_ratio);
 
   cout << "P3\n" << image_width << " " << image_height << "\n255" << endl;
+
+  auto viewport_height = 2.0;
+  auto viewport_width = aspect_ratio * viewport_height;
+  auto focal_length = 1.0;
+
+  auto origin = point3(0, 0, 0);
+  auto horizontal = vec3(viewport_width, 0, 0);
+  auto vertical = vec3(0, viewport_height, 0);
+  auto lower_left_corner =
+      origin - horizontal / 2 - vertical / 2 - vec3(0, 0, focal_length);
+
   for (int j = image_height - 1; j >= 0; --j) {
     cerr << "\rScanlines remaining: " << j << " " << flush;
     for (int i = 0; i < image_width; ++i) {
-      color pixel_color(double(i) / (image_width - 1),
-                        double(j) / (image_height - 1), 0.25);
+      auto u = double(i) / (image_width - 1);
+      auto v = double(j) / (image_height - 1);
+      ray r(origin, lower_left_corner + u * horizontal + v * vertical - origin);
+      color pixel_color = ray_color(r);
       write_color(cout, pixel_color);
     }
   }
