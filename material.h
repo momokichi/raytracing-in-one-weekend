a3347ce9958865c3b9425fb0ed7f5ca0de3ee9dc
Suzuki Kensuke <s1811349@s.tsukuba.ac.jp>
2021-12-05 10:53:31 +0900

    fuzzed metal

--- material.h
+++ material.h
@@ -34,14 +34,15 @@ class lambertian : public material {
 class metal : public material {
  public:
   color albedo;
+  double fuzz;
 
  public:
-  metal(const color& a) : albedo(a) {}
+  metal(const color& a, double f) : albedo(a), fuzz(f < 1 ? f : 1) {}
 
   virtual bool scatter(const ray& r_in, const hit_record& rec,
                        color& attenuation, ray& scattered) const {
     vec3 reflected = reflect(unit_vector(r_in.direction()), rec.normal);
-    scattered = ray(rec.p, reflected);
+    scattered = ray(rec.p, reflected + fuzz * random_in_unit_sphere());
     attenuation = albedo;
     return (dot(scattered.direction(), rec.normal) > 0);
   }
