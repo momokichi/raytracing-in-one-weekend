d9d22704e9ec2349993e7a7f37fa65b5a57b6f60
Suzuki Kensuke <s1811349@s.tsukuba.ac.jp>
2021-12-05 19:11:58 +0900

    move camera

--- main.cpp
+++ main.cpp
@@ -43,6 +43,7 @@ int main() {
   const int max_depth = 50;
 
   hittable_list world;
+  auto R = cos(pi / 4);
 
   auto material_ground = make_shared<lambertian>(color(0.8, 0.8, 0.0));
   auto material_center = make_shared<lambertian>(color(0.1, 0.2, 0.5));
@@ -53,10 +54,11 @@ int main() {
       make_shared<sphere>(point3(0.0, -100.5, -1.0), 100.0, material_ground));
   world.add(make_shared<sphere>(point3(0.0, 0.0, -1.0), 0.5, material_center));
   world.add(make_shared<sphere>(point3(-1.0, 0.0, -1.0), 0.5, material_left));
-  world.add(make_shared<sphere>(point3(-1.0, 0.0, -1.0), -0.4, material_left));
+  world.add(make_shared<sphere>(point3(-1.0, 0.0, -1.0), -0.45, material_left));
   world.add(make_shared<sphere>(point3(1.0, 0.0, -1.0), 0.5, material_right));
 
-  camera cam;
+  camera cam(point3(-2, 2, 1), point3(0, 0, -1), vec3(0, 1, 0), 20.0,
+             aspect_ratio);
 
   cout << "P3\n" << image_width << " " << image_height << "\n255" << endl;
 
